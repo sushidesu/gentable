36e56335014c627b3c0f586d15646ee354abead9
sushidesu <hugemerge@gmail.com>
2022-03-03 10:57:00 +0900

    feat: textareaを表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
+import { useCallback, useState} from "react"
+
 const App = () => {
+  const [text, setText] = useState<string>("")
+  const handleChangeText = useCallback((e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setText(e.target.value)
+  }, [])
+
   return (
-    <div>hello</div>
+    <div>
+      <h1>GENTABLE</h1>
+      <textarea value={text} onChange={handleChangeText} />
+      <textarea value={text} readOnly />
+    </div>
   )
 }
 
